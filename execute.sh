6602f7d22473d7885d861088691d300492459e58
omerkhureshi <syedomersalar@gmail.com>
2018-07-30 07:38:26 -0700

    updated execute script

--- execute.sh
+++ execute.sh
@@ -26,8 +26,14 @@ find . -type f -name '*.class' -delete
 
 find src/ -type f -iname "*.java" > sources.txt
 
-javac -classpath lib/aspectjrt-1.8.10.jar:lib/aspectjtools-1.8.10.jar:lib/loggerlib.jar:lib/controlsfx-8.40.12.jar:lib/derby.jar -g -d ./classes @sources.txt
+# compile java classes
+javac -classpath "lib/*" -g -d ./classes @sources.txt
 
-java -noverify -cp lib/aspectjrt-1.8.10.jar:lib/aspectjtools-1.8.10.jar:lib/loggerlib.jar:lib/controlsfx-8.40.12.jar:lib/derby.jar:. org.aspectj.tools.ajc.Main -source 1.5 -inpath ./classes -aspectpath ./src/ -d ./target
+#  run the aspectJ main class to instrument source code.
+java -noverify -cp lib/aspectjrt-1.8.10.jar:lib/aspectjtools-1.8.10.jar:lib/loggerlib.jar:lib/controlsfx-8.40.12.jar:lib/derby.jar:lib:lib/*:/:. org.aspectj.tools.ajc.Main -source 1.5 -inpath ./classes -aspectpath ./src/ -d ./target
 
-java -cp lib/aspectjrt-1.8.10.jar:lib/aspectjtools-1.8.10.jar:lib/loggerlib.jar:lib/controlsfx-8.40.12.jar:lib/derby.jar:./target $1
+# copy the resources files to target folder.
+cp -R resources/* target/
+
+#  Run the instrumented program
+java -cp lib:lib/*:./target $1
